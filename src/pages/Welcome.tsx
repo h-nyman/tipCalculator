5b3b0b75a770135d2535a72a7982d5f3ec8b7d17
Hanna Nyman <hanna.m.nyman@student.laurea.fi>
2025-03-17 10:03:53 +0200

    Fix text

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -18,14 +18,14 @@ const Welcome: React.FC = () => {
                     </IonToolbar>
                 </IonHeader>
                 <IonText>
-                    <p>This application has two functions: </p>
-                    <p>It can help you calculate how much your bill is when you add a certain percentage of tip.</p>
-                    <p>It can also divide the payment amount between a group of people.</p></IonText>
-                <div className="center-container">
-                    <IonButton routerLink="/tip" shape="round" fill="outline">
-                        Go to Tip Calculator
-                    </IonButton>
-                </div>
+                    <p>This application has two functions:</p>
+                    <p>It helps you calculate the total bill, including a specified tip percentage.</p>
+                    <p>It also divides the total amount among a group of people.</p></IonText>
+                    <div className="center-container">
+                        <IonButton routerLink="/tip" shape="round" fill="outline">
+                            Go to Tip Calculator
+                        </IonButton>
+                    </div>
             </IonContent>
         </IonPage>
     );
